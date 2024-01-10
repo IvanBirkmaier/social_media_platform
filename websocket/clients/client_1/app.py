37e3fbad85974b816d51e8a8c67f2d8a06c024bd
Renaturepeople <keypassrenaturepeople@gmail.com>
2024-01-10 13:34:16 +0100

    Added logs refactored code

--- websocket/clients/client_1/app.py
+++ websocket/clients/client_1/app.py
@@ -21,22 +21,3 @@ async def trigger_update(background_tasks: BackgroundTasks):
         raise HTTPException(status_code=400, detail="Keine Comment-ID angegeben")
 
 
-
-
-# from fastapi import FastAPI, HTTPException, BackgroundTasks
-# from pydantic import BaseModel
-# from src.websocket_client import send_update_to_server
-
-# app = FastAPI()
-
-# class UpdateRequest(BaseModel):
-#     comment_id: int
-
-# @app.post("/trigger_update")
-# async def trigger_update(request: UpdateRequest, background_tasks: BackgroundTasks):
-#     comment_id = request.comment_id
-#     if comment_id:
-#         background_tasks.add_task(send_update_to_server, comment_id)
-#         return {"message": "Update-Anfrage gesendet"}
-#     else:
-#         raise HTTPException(status_code=400, detail="Keine Comment-ID angegeben")
--- websocket/clients/client_1/src/websocket_client.py
+++ websocket/clients/client_1/src/websocket_client.py
@@ -29,36 +29,3 @@ def trigger_server_update():
     asyncio.get_event_loop().run_until_complete(send_update_to_server())
 
 
-
-# import asyncio
-# import websockets
-# import os
-# import json
-# import logging
-
-
-
-# # Konfigurieren des Loggings
-# logging.basicConfig(level=logging.INFO)
-# logger = logging.getLogger(__name__)
-
-
-# WEBSOCKET_SERVER_HOST = os.getenv('WEBSOCKET_SERVER_HOST', 'websocket_server')
-
-# WEBSOCKET_PORT = int(os.getenv('WEBSOCKET_PORT', 8765))
-
-
-# async def send_update_to_server(comment_id):
-#     uri = f"ws://{WEBSOCKET_SERVER_HOST}:{WEBSOCKET_PORT}"
-#     async with websockets.connect(uri) as websocket:
-#         logger.info(f"################################## MESSSAGE SENDED TO WebSocket Server Response")
-#         update_message = json.dumps({"type": "update_comment", "comment_id": comment_id})
-#         await websocket.send(update_message)
-#         response = await websocket.recv()
-#         logger.info(f"WebSocket Server Response: {response}")
-
-
-# def trigger_server_update(comment_id):
-#     asyncio.get_event_loop().run_until_complete(send_update_to_server(comment_id))
-
-
